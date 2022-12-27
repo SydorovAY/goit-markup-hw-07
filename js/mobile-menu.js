ac8395240e882b9b1b02ce10db0d48bbe4e6ef7b
Artem Sydorov <sydorovay@gmail.com>
2022-12-28 00:45:53 +0100

    scroll body menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,6 +8,7 @@
 	refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
 	function toggleMenu() {
+		document.body.classList.toggle("menu-open");
 		refs.menu.classList.toggle("is-open");
 	}
 })();
